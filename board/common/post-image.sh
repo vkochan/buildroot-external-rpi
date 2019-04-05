2105c79f6b142badb47a21b23304488000dfa326
Vadim Kochan <vadim4j@gmail.com>
2019-04-06 02:19:59 +0300

    board/rpi3: Check 64bit mode in ${O}/.config instead of post-image arg
    Get rid of post-image.sh aarch64 arg, but use $O/.config instead,
    and check BR2_aarch64=y option.
    
    Signed-off-by: Vadim Kochan <vadim4j@gmail.com>

--- board/common/post-image.sh
+++ board/common/post-image.sh
@@ -25,26 +25,7 @@ dtoverlay=pi3-miniuart-bt
 __EOF__
 		fi
 		;;
-		--aarch64)
-		# Run a 64bits kernel (armv8)
-		sed -e '/^kernel=/s,=.*,=Image,' -i "${BINARIES_DIR}/rpi-firmware/config.txt"
-		if ! grep -qE '^arm_64bit=1' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
 
-# enable 64bits support
-arm_64bit=1
-__EOF__
-		fi
-
-		# Enable uart console
-		if ! grep -qE '^enable_uart=1' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-
-# enable rpi3 ttyS0 serial console
-enable_uart=1
-__EOF__
-		fi
-		;;
 		--gpu_mem_256=*|--gpu_mem_512=*|--gpu_mem_1024=*)
 		# Set GPU memory
 		gpu_mem="${arg:2}"
@@ -55,6 +36,29 @@ __EOF__
 done
 
 rm -rf "${GENIMAGE_TMP}"
+
+if grep -qs "BR2_aarch64=y" ${CONFIG}; then
+	GENIMAGE_CFG="${SCRIPT_DIR}/../${BOARD_NAME}/genimage-64.cfg"
+
+	# Run a 64bits kernel (armv8)
+	sed -e '/^kernel=/s,=.*,=Image,' -i "${BINARIES_DIR}/rpi-firmware/config.txt"
+	if ! grep -qE '^arm_64bit=1' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# enable 64bits support
+arm_64bit=1
+__EOF__
+	fi
+
+	# Enable uart console
+	if ! grep -qE '^enable_uart=1' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# enable rpi3 ttyS0 serial console
+enable_uart=1
+__EOF__
+	fi
+fi
 
 if grep -qs "BR2_TARGET_UBOOT=y" ${CONFIG}; then
 	$MKIMAGE -A arm -O linux -T script -C none -n boot.scr \
